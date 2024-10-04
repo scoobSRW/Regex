3835f7c136261fa9602df03a9650f7fe528f02b0
scoobSRW <166456822+scoobSRW@users.noreply.github.com>
2024-10-03 21:27:48 -0500

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -3,7 +3,6 @@ import re
 text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
 exclude_domain = "exclude.com"
 
-# Regex to extract all email addresses except those from exclude_domain
 emails = re.findall(r"\b[A-Za-z0-9._%+-]+@(?!{})[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b".format(re.escape(exclude_domain)), text)
 
 print(emails)
